60525bb22c6034905737c00cb3aadf4f41446a52
Alex <zuedev@gmail.com>
2022-07-02 14:42:54 +0000

    test with static ip

--- google.tf
+++ google.tf
@@ -2,9 +2,14 @@ provider "google" {
   credentials = var.google_credentials
 }
 
-resource "google_compute_instance" "default" {
+resource "google_compute_address" "test321" {
+  project = "zuedev2022"
+  name    = "test321"
+}
+
+resource "google_compute_instance" "test321" {
   project      = "zuedev2022"
-  name         = "test"
+  name         = "test321"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
@@ -17,6 +22,8 @@ resource "google_compute_instance" "default" {
   network_interface {
     network = "default"
 
-    access_config {}
+    access_config {
+      nat_ip = google_compute_address.test321.address
+    }
   }
 }
