bab41c851c65f8aac96a543715c8e26743896b2a
Alex <zuedev@gmail.com>
2022-07-02 14:52:20 +0000

    remove test resources from google tf

--- google.tf
+++ google.tf
@@ -4,27 +4,3 @@ provider "google" {
   zone        = "europe-west2-a"
   credentials = var.google_credentials
 }
-
-resource "google_compute_address" "test321" {
-  project = "zuedev2022"
-  name    = "test321"
-}
-
-resource "google_compute_instance" "test321" {
-  name         = "test321"
-  machine_type = "e2-medium"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      nat_ip = google_compute_address.test321.address
-    }
-  }
-}
