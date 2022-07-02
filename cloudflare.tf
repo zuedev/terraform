397d79cde5e201a997ebcc37f7ed01ed39f12470
Alex <zuedev@gmail.com>
2022-07-02 13:44:31 +0000

    add all applicable settings for zue.dev

--- cloudflare.tf
+++ cloudflare.tf
@@ -16,18 +16,35 @@ resource "cloudflare_zone_settings_override" "zue-dot-dev-settings" {
     always_use_https         = "on"
     automatic_https_rewrites = "on"
     brotli                   = "on"
-    browser_check            = "on"
-    development_mode         = "off"
-    early_hints              = "on"
-    email_obfuscation        = "on"
+    cname_flattening         = "flatten_all"
+    email_obfuscation        = "off"
+    h2_prioritization        = "on"
     hotlink_protection       = "off"
     http2                    = "on"
     http3                    = "on"
+    image_resizing           = "open"
+    min_tls_version          = "1.3"
     minify {
       css  = "on"
       js   = "on"
       html = "on"
     }
+    mirage                   = "on"
+    opportunistic_encryption = "on"
+    opportunistic_onion      = "on"
+    polish                   = "lossless"
+    security_header {
+      enabled            = true
+      preload            = true
+      include_subdomains = true
+      nosniff            = true
+    }
+    security_level = "high"
+    ssl            = "strict"
+    tls_1_3        = "zrt"
+    webp           = "on"
+    websockets     = "on"
+    zero_rtt       = "on"
   }
 }
 
