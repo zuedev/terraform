ada6a6ab34201a7689b1cbd7884b8d02ddec26c0
Alex <zuedev@gmail.com>
2022-07-02 10:43:16 +0000

    initial commit

---
+++ cloudflare.tf
@@ -0,0 +1,8 @@
+provider "cloudflare" {
+  email   = var.cloudflare_email
+  api_key = var.cloudflare_api_key
+}
+
+resource "cloudflare_zone" "zue-dot-dev" {
+    zone = "zue.dev"
+}
---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+  cloud {
+    organization = "zuedev"
+
+    workspaces {
+      name = "terraform"
+    }
+  }
+}
