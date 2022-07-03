fb22607e31b3e5e2e4cdb01d7bf1e71224ef3079
Alex <zuedev@gmail.com>
2022-07-03 16:07:47 +0000

    add unnamed group discord redirect

---
+++ cloudflare_unnamed-dot-group_pagerules.tf
@@ -0,0 +1,11 @@
+resource "cloudflare_page_rule" "unnamed-dot-group-discord-redirect" {
+  zone_id = cloudflare_zone.unnamed-dot-group.id
+  target  = "*${cloudflare_zone.unnamed-dot-group.zone}/discord"
+
+  actions {
+    forwarding_url {
+      url         = "https://discord.com/invite/JnDGdHp9JV"
+      status_code = "301"
+    }
+  }
+}
