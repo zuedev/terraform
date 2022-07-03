bf25dc942b1c16df2632c967a090b9d5a4a2b050
Alex <zuedev@gmail.com>
2022-07-03 15:51:12 +0000

    copy cf settings from zue-dot-dev

--- cloudflare_unnamed-dot-group.tf
+++ cloudflare_unnamed-dot-group.tf
@@ -3,6 +3,45 @@ resource "cloudflare_zone" "unnamed-dot-group" {
   plan = "free"
 }
 
+resource "cloudflare_zone_settings_override" "unnamed-dot-group-settings" {
+  zone_id = cloudflare_zone.unnamed-dot-group.id
+  settings {
+    always_online            = "on"
+    always_use_https         = "on"
+    automatic_https_rewrites = "on"
+    brotli                   = "on"
+    cname_flattening         = "flatten_all"
+    email_obfuscation        = "off"
+    h2_prioritization        = "on"
+    hotlink_protection       = "off"
+    http2                    = "on"
+    http3                    = "on"
+    image_resizing           = "open"
+    min_tls_version          = "1.3"
+    minify {
+      css  = "on"
+      js   = "on"
+      html = "on"
+    }
+    mirage                   = "on"
+    opportunistic_encryption = "on"
+    opportunistic_onion      = "on"
+    polish                   = "lossless"
+    security_header {
+      enabled            = true
+      preload            = true
+      include_subdomains = true
+      nosniff            = true
+    }
+    security_level = "high"
+    ssl            = "strict"
+    tls_1_3        = "zrt"
+    webp           = "on"
+    websockets     = "on"
+    zero_rtt       = "on"
+  }
+}
+
 resource "cloudflare_record" "unnamed-dot-group-domain-forward-a-1" {
   zone_id = cloudflare_zone.unnamed-dot-group.id
   name    = "@"
