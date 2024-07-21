f3c1cde988958f696d25afd61eac44d774a3fff6
Alex <zuedev@gmail.com>
2024-07-21 04:21:51 +0100

    better org

---
+++ src/cloudflare_zue.dev_sovereign.tf
@@ -0,0 +1,27 @@
+# main record for the sovereign server
+resource "cloudflare_record" "zue-dot-dev_sovereign_a" {
+  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
+
+  comment = "TF: sovereign server main"
+
+  type    = "A"
+  name    = "sovereign"
+  value   = "81.129.128.138"
+  proxied = false
+
+  allow_overwrite = true
+}
+
+# wildcard
+resource "cloudflare_record" "zue-dot-dev_sovereign_wildcard" {
+  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
+
+  comment = "TF: sovereign server wildcard"
+
+  type    = "CNAME"
+  name    = "*.sovereign"
+  value   = "sovereign.zue.dev"
+  proxied = false
+
+  allow_overwrite = true
+}
