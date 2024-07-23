a43ea8fbbf29ef34c233f1d8173a0eb80083b9c4
Alex <zuedev@gmail.com>
2024-07-23 14:46:48 +0100

    add docs

---
+++ src/cloudflare_uagpmc.com_dns_docs.tf
@@ -0,0 +1,12 @@
+resource "cloudflare_record" "uagpmc-dot-com_arma3_cname" {
+  zone_id = data.cloudflare_zones.uagpmc-dot-com.zones.0.id
+
+  comment = "TF: docs cf pages"
+
+  type    = "CNAME"
+  name    = "docs"
+  value   = "docs-uagpmc-com.pages.dev"
+  proxied = true
+
+  allow_overwrite = true
+}
