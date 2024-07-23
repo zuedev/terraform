56b52b2c910f6a12d6a0fa2104df47fd11146578
Alex <zuedev@gmail.com>
2024-07-23 14:19:02 +0100

    use try except

--- scripts/cloudflare_delete_all_dns_records_from_zone.py
+++ scripts/cloudflare_delete_all_dns_records_from_zone.py
@@ -13,10 +13,13 @@ response = requests.get(url, headers=headers)
 response.raise_for_status()
 
 for record in response.json()["result"]:
-    record_id = record["id"]
-    delete_url = f"https://api.cloudflare.com/client/v4/zones/{zoneid}/dns_records/{record_id}"
-    response = requests.delete(delete_url, headers=headers)
-    response.raise_for_status()
-    print(f"Deleted record {record_id}")
+    try:
+        record_id = record["id"]
+        delete_url = f"https://api.cloudflare.com/client/v4/zones/{zoneid}/dns_records/{record_id}"
+        response = requests.delete(delete_url, headers=headers)
+        response.raise_for_status()
+        print(f"Deleted record {record_id}")
+    except Exception as e:
+        print(f"Failed to delete record {record_id} with error: {e}")
 
 print("All records deleted successfully for zone", zoneid)
