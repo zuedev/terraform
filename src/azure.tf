672538df274cd9037a2bb860fc83ac1697426b2e
zuedev <zuedev@gmail.com>
2024-08-22 03:44:40 +0100

    add sub id

--- src/azure.tf
+++ src/azure.tf
@@ -4,9 +4,10 @@ variable "azure_client_secret" {
 }
 
 provider "azurerm" {
-  tenant_id     = "ab31a38f-3d03-4dbd-be8c-03a48c54e845"
-  client_id     = "d56ed96c-0c52-44c0-b117-2d137b4757ad"
-  client_secret = var.azure_client_secret
+  subscription_id = "a31532fe-3764-4810-a036-79d09f9492ab"
+  tenant_id       = "ab31a38f-3d03-4dbd-be8c-03a48c54e845"
+  client_id       = "d56ed96c-0c52-44c0-b117-2d137b4757ad"
+  client_secret   = var.azure_client_secret
 
   features {}
 }
