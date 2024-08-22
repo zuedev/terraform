1f9f617679767fc2120d97dde8b4b72c60fa0dd5
zuedev <zuedev@gmail.com>
2024-08-22 03:53:52 +0100

    add more

--- src/azure_web-tests.tf
+++ src/azure_web-tests.tf
@@ -10,7 +10,11 @@ resource "azurerm_application_insights_standard_web_test" "zue-dot-dev" {
   resource_group_name     = data.azurerm_resource_group.terraform_uk_south.name
   location                = data.azurerm_resource_group.terraform_uk_south.location
   application_insights_id = azurerm_application_insights.zue-dot-dev.id
-  geo_locations           = ["emea-ru-msa-edge"]
+  geo_locations = [
+    "emea-ru-msa-edge",
+    "us-va-ash-azr",
+    "emea-au-syd-edge"
+  ]
 
   request {
     url = "https://zue.dev"
