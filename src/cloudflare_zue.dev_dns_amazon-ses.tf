9087fe3de481d122dedbca0ea62a3c1caee6848f
Alex <zuedev@gmail.com>
2024-10-19 15:10:30 +0100

    add gitlab ses

--- src/cloudflare_zue.dev_dns_amazon-ses.tf
+++ src/cloudflare_zue.dev_dns_amazon-ses.tf
@@ -1,67 +1,40 @@
 # DomainKeys Identified Mail (DKIM)
 
-resource "cloudflare_record" "zue-dot-dev_amazon-ses_dkim-cname-1" {
+resource "cloudflare_record" "gitlab-zue-dot-dev_amazon-ses_dkim-cname-1" {
   zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
 
-  comment = "TF: apex amazon ses"
+  comment = "TF: gitlab amazon ses"
 
   type    = "CNAME"
-  name    = "2eytzuztn325iolhi3twrxi3jykyb5jg._domainkey"
-  value   = "2eytzuztn325iolhi3twrxi3jykyb5jg.dkim.amazonses.com"
+  name    = "vlf574tofoowm5pso22sng2y7x6nds7u._domainkey.gitlab"
+  value   = "vlf574tofoowm5pso22sng2y7x6nds7u.dkim.amazonses.com"
   proxied = false
 
   allow_overwrite = true
 }
 
-resource "cloudflare_record" "zue-dot-dev_amazon-ses_dkim-cname-2" {
+resource "cloudflare_record" "gitlab-zue-dot-dev_amazon-ses_dkim-cname-2" {
   zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
 
-  comment = "TF: apex amazon ses"
+  comment = "TF: gitlab amazon ses"
 
   type    = "CNAME"
-  name    = "iltwjfurohirkesaa5dtamzufre4bh77._domainkey"
-  value   = "iltwjfurohirkesaa5dtamzufre4bh77.dkim.amazonses.com"
+  name    = "nuqrsf36knuzbtpbkawq75injexk66gp._domainkey.gitlab"
+  value   = "nuqrsf36knuzbtpbkawq75injexk66gp.dkim.amazonses.com"
   proxied = false
 
   allow_overwrite = true
 }
 
-resource "cloudflare_record" "zue-dot-dev_amazon-ses_dkim-cname-3" {
+resource "cloudflare_record" "gitlab-zue-dot-dev_amazon-ses_dkim-cname-3" {
   zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
 
-  comment = "TF: apex amazon ses"
+  comment = "TF: gitlab amazon ses"
 
   type    = "CNAME"
-  name    = "pzvvblxyoxhg24ydtkqiotnltrhfroqu._domainkey"
-  value   = "pzvvblxyoxhg24ydtkqiotnltrhfroqu.dkim.amazonses.com"
+  name    = "6qgizueeseamde54h4a4x67o6jhj3exd._domainkey.gitlab"
+  value   = "6qgizueeseamde54h4a4x67o6jhj3exd.dkim.amazonses.com"
   proxied = false
-
-  allow_overwrite = true
-}
-
-# Custom MAIL FROM domain
-
-resource "cloudflare_record" "zue-dot-dev_amazon-ses_custom-mail-from-mx" {
-  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
-
-  comment = "TF: apex amazon ses"
-
-  type     = "MX"
-  name     = "aws-ses"
-  value    = "feedback-smtp.eu-west-2.amazonses.com"
-  priority = 10
-
-  allow_overwrite = true
-}
-
-resource "cloudflare_record" "zue-dot-dev_amazon-ses_custom-mail-from-spf" {
-  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
-
-  comment = "TF: apex amazon ses"
-
-  type  = "TXT"
-  name  = "aws-ses"
-  value = "v=spf1 include:amazonses.com -all"
 
   allow_overwrite = true
 }
@@ -69,13 +42,13 @@ resource "cloudflare_record" "zue-dot-dev_amazon-ses_custom-mail-from-spf" {
 # Domain-based Message Authentication, Reporting, and Conformance (DMARC)
 
 # REPLACED WITH CLOUDFLARE DMARC MANAGEMENT
-# resource "cloudflare_record" "zue-dot-dev_amazon-ses_dmarc" {
+# resource "cloudflare_record" "gitlab-zue-dot-dev_amazon-ses_dmarc" {
 #   zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
 
-#   comment = "TF: apex amazon ses"
+#   comment = "TF: gitlab amazon ses"
 
 #   type  = "TXT"
-#   name  = "_dmarc"
+#   name  = "_dmarc.gitlab"
 #   value = "v=DMARC1; p=none;"
 
 #   allow_overwrite = true
---
+++ src/cloudflare_zue.dev_dns_amazon-ses_gitlab.tf
@@ -0,0 +1,82 @@
+# DomainKeys Identified Mail (DKIM)
+
+resource "cloudflare_record" "zue-dot-dev_amazon-ses_dkim-cname-1" {
+  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
+
+  comment = "TF: apex amazon ses"
+
+  type    = "CNAME"
+  name    = "2eytzuztn325iolhi3twrxi3jykyb5jg._domainkey"
+  value   = "2eytzuztn325iolhi3twrxi3jykyb5jg.dkim.amazonses.com"
+  proxied = false
+
+  allow_overwrite = true
+}
+
+resource "cloudflare_record" "zue-dot-dev_amazon-ses_dkim-cname-2" {
+  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
+
+  comment = "TF: apex amazon ses"
+
+  type    = "CNAME"
+  name    = "iltwjfurohirkesaa5dtamzufre4bh77._domainkey"
+  value   = "iltwjfurohirkesaa5dtamzufre4bh77.dkim.amazonses.com"
+  proxied = false
+
+  allow_overwrite = true
+}
+
+resource "cloudflare_record" "zue-dot-dev_amazon-ses_dkim-cname-3" {
+  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
+
+  comment = "TF: apex amazon ses"
+
+  type    = "CNAME"
+  name    = "pzvvblxyoxhg24ydtkqiotnltrhfroqu._domainkey"
+  value   = "pzvvblxyoxhg24ydtkqiotnltrhfroqu.dkim.amazonses.com"
+  proxied = false
+
+  allow_overwrite = true
+}
+
+# Custom MAIL FROM domain
+
+resource "cloudflare_record" "zue-dot-dev_amazon-ses_custom-mail-from-mx" {
+  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
+
+  comment = "TF: apex amazon ses"
+
+  type     = "MX"
+  name     = "aws-ses"
+  value    = "feedback-smtp.eu-west-2.amazonses.com"
+  priority = 10
+
+  allow_overwrite = true
+}
+
+resource "cloudflare_record" "zue-dot-dev_amazon-ses_custom-mail-from-spf" {
+  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
+
+  comment = "TF: apex amazon ses"
+
+  type  = "TXT"
+  name  = "aws-ses"
+  value = "v=spf1 include:amazonses.com -all"
+
+  allow_overwrite = true
+}
+
+# Domain-based Message Authentication, Reporting, and Conformance (DMARC)
+
+# REPLACED WITH CLOUDFLARE DMARC MANAGEMENT
+# resource "cloudflare_record" "zue-dot-dev_amazon-ses_dmarc" {
+#   zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
+
+#   comment = "TF: apex amazon ses"
+
+#   type  = "TXT"
+#   name  = "_dmarc"
+#   value = "v=DMARC1; p=none;"
+
+#   allow_overwrite = true
+# }
